48722abd5681579a6c54b78db6896bbd53c41fb9
varribass01 <varribass01@iesvjp.es>
2026-09-25 17:23:03 +0200

    Meto ejercicios 9 y 23 del tema 2

---
+++ ejt2/ej09.java
@@ -0,0 +1,19 @@
+
+package ejerciciot2;
+
+/**
+ *
+ * @author alumno
+ */
+public class ej09 {
+    public static void main(String[] args) {
+    //declaro variables
+    float PI = 3.141592F;
+    float r = 3.55F;
+    //aplico la fórmula de la longitud de las variables
+    float resultado = 2 * PI * r;
+    //Hago un sout para enseñar el resultado
+    System.out.println("La longitud de la circunferencia con radio 3,55 es: " + resultado);
+    }
+    
+}
